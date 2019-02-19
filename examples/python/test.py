79ce5d5265c9976ec67d4ea1373bb88bad3ecdb2
Aidan Feldman <aidan.l.feldman@census.gov>
2019-02-19 15:40:21 -0500

    add Python connection example

---
+++ examples/python/test.py
@@ -0,0 +1,7 @@
+from pyhive import hive
+
+cursor = hive.connect("localhost").cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS people (name string)")
+cursor.execute("INSERT INTO people (name) VALUES ('Aidan')")
+cursor.execute("SELECT * FROM people LIMIT 10")
+print(cursor.fetchone())
