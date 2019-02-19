23f90a7cad2a00fb5aa521f5d450e3651ce13fbf
Aidan Feldman <aidan.l.feldman@census.gov>
2019-02-19 17:39:47 -0500

    move R setup into a function

--- examples/r/setup.R
+++ examples/r/setup.R
@@ -6,17 +6,20 @@ if (!require(RJDBC)) {
 
 library(rJava)
 
-# https://mapr.com/support/s/article/How-to-connect-R-to-Hiveserver2-using-hive-jdbc?language=en_US
 
-cp=c(
-  "../../apache-hive-3.1.1-bin/jdbc/hive-jdbc-3.1.1-standalone.jar",
-  "../../hadoop-3.2.0/share/hadoop/common/hadoop-common-3.2.0.jar"
-)
-.jinit(classpath=cp)
-drv <- JDBC(
-  "org.apache.hive.jdbc.HiveDriver",
-  "../../apache-hive-3.1.1-bin/lib/hive-jdbc-3.1.1.jar",
-  identifier.quote="`"
-)
+getHiveConn <- function() {
+  # https://mapr.com/support/s/article/How-to-connect-R-to-Hiveserver2-using-hive-jdbc?language=en_US
 
-conn <- dbConnect(drv, "jdbc:hive2://localhost:10000/;") #, "<user>", "<password>")
+  cp=c(
+    "../../apache-hive-3.1.1-bin/jdbc/hive-jdbc-3.1.1-standalone.jar",
+    "../../hadoop-3.2.0/share/hadoop/common/hadoop-common-3.2.0.jar"
+  )
+  .jinit(classpath=cp)
+  drv <- JDBC(
+    "org.apache.hive.jdbc.HiveDriver",
+    "../../apache-hive-3.1.1-bin/lib/hive-jdbc-3.1.1.jar",
+    identifier.quote="`"
+  )
+
+  dbConnect(drv, "jdbc:hive2://localhost:10000/;") #, "<user>", "<password>")
+}
--- examples/r/test.R
+++ examples/r/test.R
@@ -1,5 +1,7 @@
 source("setup.R")
 
+conn <- getHiveConn()
+
 db <- dbGetQuery(conn, "show databases")
 print(db)
 
